84b660dbb4da626fe2b45d2b7ca715c437607f09
WeiZhang418 <zhangwei418@gmail.com>
2016-04-25 19:53:22 +0800

    Update CalculatorTest.java

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest { 
 
 	private static Calculator calculator=new Calculator();
 	@Before
